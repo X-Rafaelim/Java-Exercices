5d51f2fdb32de4b142c0a25ab8db88aad6c90a31
Rafael Barcelos <rafaelteixeirabarcelos@enta.pt>
2020-11-18 11:54:53 -0100

    Atualização comentários

--- Ex_18_11_2020/GesAlunos.java
+++ Ex_18_11_2020/GesAlunos.java
@@ -3,10 +3,10 @@ import java.util.concurrent.TransferQueue;
 
 public class GesAlunos {
     public static void main(String[] args) {
+
         ArrayList<Aluno> aluno = new ArrayList<>();
-        ArrayList<Turma> turma = new ArrayList<>();
 
-
+        //objetos criados com seus criterios
         Aluno alun1 = new Aluno("Rafael",19,"rakitir@gmail.com", 17.10);
         Aluno alun2 = new Aluno("Mario",90,"mariogod@gmail.com", 15.10);
         Aluno alun3 = new Aluno("Roberto",30,"roberoplays@gmail.com", 14.90);
@@ -19,21 +19,27 @@ public class GesAlunos {
         Aluno alun10 = new Aluno("Emiliano",23,"firmamarreta@gmail.com", 12.90);
 
 
-
+        //adicionar os objetos as uma lista de alunos que vai ser usada no objeto da turma1
         aluno.add(alun1);
         aluno.add(alun2);
         aluno.add(alun3);
         aluno.add(alun4);
         aluno.add(alun5);
+        //criação do objeto turma 1
         Turma turma1 = new Turma(1,aluno);
+
+        //criação dad lista anlunost2 para que seja usada na segunda turma
         ArrayList<Aluno> alunost2 = new ArrayList<Aluno>();
 
+        //adicionando os objetos a lista alunost2 para serem usado para a turma 2 
         alunost2.add(alun6);
         alunost2.add(alun7);
         alunost2.add(alun8);
         alunost2.add(alun9);
         alunost2.add(alun10);
+        //criação do objeto  turma 2
         Turma turma2 = new Turma(2,alunost2);
+
         System.out.println("Pessoas menores");
         System.out.println(contaMenores(turma2));
         System.out.println("---------------------------------");
@@ -43,7 +49,7 @@ public class GesAlunos {
 
 
     }
-
+    //Mostra o alunos menores de 18 anos de idade
     public static int contaMenores(Turma turma){
         int contador = 0;
         for (int i = 0; i< turma.getAlunos().size();i++){
@@ -52,11 +58,10 @@ public class GesAlunos {
                 contador++;
             }
         }
-
         return contador;
-
     }
 
+    //Mostra os alunos com nota mais ou igual a 10.00
     public static int contaMedia(Turma turma){
         int contador = 0;
         for (int i = 0; i< turma.getAlunos().size();i++){
