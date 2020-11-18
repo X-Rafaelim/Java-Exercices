1c64f497d2a6ad4a56c5fc726b96226503816c36
Rafael Barcelos <rafaelteixeirabarcelos@enta.pt>
2020-11-18 09:48:45 -0100

    Turma

---
+++ Ex_18_11_2020/Turma.java
@@ -0,0 +1,27 @@
+public class Turma {
+    private int id_alunos;
+    private String alunos;
+
+
+    //construtor
+    public Turma(int id_alunos, String alunos){
+        this.id_alunos = id_alunos;
+        this.alunos = alunos;
+    }
+
+    //setters
+    public void setId_alunos(int id_alunos){
+        this.id_alunos = id_alunos;
+    }
+    public void setAlunos(String alunos){
+        this.alunos = alunos;
+    }
+
+    //getters
+
+
+}
+
+
+
+
